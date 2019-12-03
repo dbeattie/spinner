114e563a934626c0eee2a28007489a70ce90595d
dbeattie <darren@fettle.ca>
2019-12-03 17:46:14 +0000

    added \n to escape the spinner to a new line
    slowed the spinner down a bit to make it easier to observe what's happening

--- spinner1.js
+++ spinner1.js
@@ -5,34 +5,39 @@ setTimeout(() => {
 
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
 }, 500);
 
 setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 800);
+
+setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
-}, 700);
+}, 1200);
 
 setTimeout(() => {
   process.stdout.write('\r|   '); 
-}, 900);
+}, 1500);
+
 setTimeout(() => {
-  process.stdout.write('\r/  '); 
-}, 1100);
+  process.stdout.write('\r/   '); 
+}, 1800);
 
 setTimeout(() => {
   process.stdout.write('\r-   '); 
-}, 1300);
+}, 2100);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
-}, 1500);
+}, 2400);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|\   '); 
-}, 1700);
+}, 2700);
+
+setTimeout(() => {
+  // Need to escape the line in Vagrant
+  process.stdout.write('\n'); 
+}, 3000);
