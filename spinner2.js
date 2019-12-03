bfe70dd8fbca5fced938fd00a484fdbf07469dd9
dbeattie <darren@fettle.ca>
2019-12-03 18:36:24 +0000

    spaced the code out a bit

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,8 @@
 // ARRAY WITH \r COMMAND FOR SPINNER CREATION AND \n TO EXIT THE LINE AT THE END
 const cycle = ["\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|\   ", "\n"];
+
 let delay = 0;
+
 for (const char of cycle) {
   setTimeout(() => {
     process.stdout.write(char);
